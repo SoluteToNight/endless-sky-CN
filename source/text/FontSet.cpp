b214aa390d8a2a22230c4ffbd4e7b95c5b2daf87
SoluteToNight <80676730+SoluteToNight@users.noreply.github.com>
2026-06-27 18:15:04 +0800

    feat(text): Implement FreeType CJK glyph rendering with texture atlas

--- source/text/FontSet.cpp
+++ source/text/FontSet.cpp
@@ -27,10 +27,10 @@ namespace {
 
 
 
-void FontSet::Add(const filesystem::path &path, int size)
+void FontSet::Add(const vector<filesystem::path> &paths, int size)
 {
 	if(!fonts.contains(size))
-		fonts[size].Load(path);
+		fonts[size].Load(paths, size);
 }
 
 
